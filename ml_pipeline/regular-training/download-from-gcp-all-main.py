c65182517f75e1be9f25c7c1285c50c79638e1c4
pitta-bread <peterbeardsmore@hotmail.co.uk>
2023-05-23 14:53:17 +0100

    downloads from cloud

---
+++ ml_pipeline/regular-training/download-from-gcp-all-main.py
@@ -0,0 +1,38 @@
+from google.cloud import storage
+import datetime
+import os
+
+
+storage_client = storage.Client(project="home-assistant-pb96")
+
+bucket_name = 'home-assistant-pb96-tapo'
+
+destination_folder = f"data/all-main-gcp-{datetime.date.today().strftime('%Y%m%d')}"
+os.makedirs(destination_folder, exist_ok=True)
+
+for name in ["bed", "missing", "rug", "somewhere"]:
+    os.makedirs(f"{destination_folder}/class_{name}", exist_ok=True)
+
+
+# download all files in bucket
+blobs = storage_client.list_blobs(bucket_name)
+total_size = 0
+for blob in blobs:
+    # print(blob.name)
+    # print(blob.size)
+    total_size += blob.size
+    # blob.download_to_filename(f"{destination_folder}/{blob.name}")
+
+print(total_size / 1000000)
+
+# download all the files in the bucket with the prefix main-dataset
+blobs = storage_client.list_blobs(bucket_name, prefix="main-dataset/")
+download_progress = 0
+previous_download_progress = 0
+for blob in blobs:
+    name = "/".join(blob.name.split("/")[1:])
+    download_progress += blob.size
+    if int(download_progress) != previous_download_progress:
+        print(download_progress)
+        previous_download_progress = int(download_progress)
+    blob.download_to_filename(f"{destination_folder}/{name}")
